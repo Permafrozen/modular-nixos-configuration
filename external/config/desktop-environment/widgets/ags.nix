379a6d4c631b6fb026c42292ba8d4b68609eb072
Permafrozen <m8tt8o@gmail.com>
2024-09-16 19:23:53 +0200

    added ags config to ags directory under external/desk.../wid.../*

---
+++ external/config/desktop-environment/widgets/ags.nix
@@ -0,0 +1,26 @@
+{ inputs, config, pkgs, ... }:
+
+let
+  vars = import ../../../../core/system/variables.nix;
+in
+{
+  home-manager.sharedModules = [{
+    # add the home manager module
+    imports = [ inputs.ags.homeManagerModules.default ];
+
+    programs.ags = {
+      enable = true;
+
+      # null or path, leave as null if you don't want hm to manage the config
+      configDir = null;
+
+      # additional packages to add to gjs's runtime
+      extraPackages = with pkgs; [
+        gtksourceview
+        webkitgtk
+        accountsservice
+      ];
+    };
+  }];
+}
+
