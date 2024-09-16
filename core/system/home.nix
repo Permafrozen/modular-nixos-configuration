0029751730ef7a5f529a7891083525b064ea1cc4
Permafrozen <m8tt8o@gmail.com>
2024-09-16 19:16:48 +0200

    added ags test to home.nix

--- core/system/home.nix
+++ core/system/home.nix
@@ -1,4 +1,4 @@
-{ config, pkgs, ... }:
+{ inputs, config, pkgs, ... }:
 
 let
   vars = import ./variables.nix;
@@ -13,4 +13,23 @@ in
 
   # Let Home Manager install and manage itself.
   programs.home-manager.enable = true;
+
+  # Temporary AGS config
+
+  # add the home manager module
+  imports = [ inputs.ags.homeManagerModules.default ];
+
+  programs.ags = {
+    enable = true;
+
+    # null or path, leave as null if you don't want hm to manage the config
+    configDir = ../ags;
+
+    # additional packages to add to gjs's runtime
+    extraPackages = with pkgs; [
+      gtksourceview
+      webkitgtk
+      accountsservice
+    ];
+  };
 }
